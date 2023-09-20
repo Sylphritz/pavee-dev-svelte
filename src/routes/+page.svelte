59f5cbe196770710fdd4f65474655b712ed57db1
Sylphritz <3182197+Sylphritz@users.noreply.github.com>
2023-09-20 15:10:33 +0700

    Enable prerender on the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,6 @@
 	import ContactSection from '$lib/components/LandingPage/ContactSection/ContactSection.svelte'
 	import HeroSection from '$lib/components/LandingPage/HeroSection/HeroSection.svelte'
 	import WorkSection from '$lib/components/LandingPage/WorkSection/WorkSection.svelte'
-	import type { PageData } from './$types'
-
-	// export let data: PageData
 </script>
 
 <HeroSection />
