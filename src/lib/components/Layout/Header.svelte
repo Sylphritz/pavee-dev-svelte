cdafde09a7df036589e684929c9683673e23699d
Sylphritz <3182197+Sylphritz@users.noreply.github.com>
2023-10-09 15:20:40 +0700

    Make the navigation bar sticky on scrolling away

--- src/lib/components/Layout/Header.svelte
+++ src/lib/components/Layout/Header.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import HeaderNavigation from './Header/HeaderNavigation.svelte'
+	import { stickyClass } from '$lib/actions/stickyClass'
 </script>
 
-<header class="lg:py-3 md:py-1 flex flex-row justify-between md:justify-start items-baseline gap-3">
+<header
+	class="py-3 flex flex-row justify-between md:justify-start items-baseline gap-3 -top-px sticky z-20 transition-all"
+	use:stickyClass={'pinned'}
+>
 	<a
 		href="/"
 		class="inline-block text-2xl font-bold bg-gradient-to-r from-red-500 to-red-400 bg-clip-text text-transparent hover:text-transparent font-['Lexend_Variable'] tracking-tight"
